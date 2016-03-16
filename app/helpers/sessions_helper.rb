b32a83c07d000dfbb15acbb8121e04b81009da0c
Obtuse <theobtuseautodidact@gmail.com>
2016-03-16 14:48:26 -0600

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,13 +12,17 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     if (user_id = session[:user_id])
     # if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     elsif (user_id = cookies.signed[:user_id])
     # elsif cookies.signed[:user_id]
-      # raise # The tests still pass, so this branch is currently untested. 
+      # raise # The tests still pass, so this branch is currently untested.
       user = User.find_by(id: user_id)
       # user = User.find_by(id: cookies.signed[:user_id])
       if user && user.authenticated?(cookies[:remember_token])
@@ -43,5 +47,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
   end
 end
